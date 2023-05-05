b0c9d2707565005aae401d58607a012631e118c3
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-05-05 13:06:15 +0600

    add static

--- src/getter-setter.ts
+++ src/getter-setter.ts
@@ -1,46 +1,46 @@
-class BankAccount {
-    id: number;
-    name: string;
-    private _balance: number
+// class BankAccount {
+//     id: number;
+//     name: string;
+//     private _balance: number
 
 
-    constructor(id: number, name: string, balance: number) {
-        this.id = id;
-        this.name = name;
-        this._balance = balance;
-    }
+//     constructor(id: number, name: string, balance: number) {
+//         this.id = id;
+//         this.name = name;
+//         this._balance = balance;
+//     }
 
-    // getter
-    get balance(): number {
-        return this._balance
-    }
+//     // getter
+//     get balance(): number {
+//         return this._balance
+//     }
 
-    getBalance(): number {
-        return this._balance
-    }
+//     getBalance(): number {
+//         return this._balance
+//     }
 
-    // setter
-    set deposit(amount: number) {
-        this._balance = this._balance + amount
-    }
+//     // setter
+//     set deposit(amount: number) {
+//         this._balance = this._balance + amount
+//     }
 
-    addDeposit(amount: number) {
-        this._balance = this._balance + amount
-    }
+//     addDeposit(amount: number) {
+//         this._balance = this._balance + amount
+//     }
 
 
-}
-
-// class StudentAccount extends BankAccount{
-// test(){
-//     this._balance
-// }
 // }
 
+// // class StudentAccount extends BankAccount{
+// // test(){
+// //     this._balance
+// // }
+// // }
 
-const myAccount = new BankAccount(444, "nahid", 20)
 
-console.log(myAccount.balance);
-myAccount.deposit=30
+// const myAccount = new BankAccount(444, "nahid", 20)
 
-console.log(myAccount.balance);
+// console.log(myAccount.balance);
+// myAccount.deposit=30
+
+// console.log(myAccount.balance);
